75947998a9aaa6400032d68b3c913a15d054df82
oconnorK95 <55146318+oconnorK95@users.noreply.github.com>
2020-10-19 19:51:28 +0100

    Initial Commit

---
+++ 3d Graphics/Assets/Model.cs
@@ -0,0 +1,340 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Model 
+{
+    public enum default_primitives {  Cube }
+    public enum letter { K }
+
+    public List<Vector3> _vertices;
+    public List<int> _index_list;
+    public List<Vector2> _texture_coordinates;
+    public List<int> _texture_index_list;
+    public List<Vector3> _face_normals;
+
+    public Model(default_primitives shape)
+    {
+        switch (shape)
+        {
+            case default_primitives.Cube:
+                initialize_lists();
+                add_cube_vertices();
+                add_texture_coordinates();
+                add_indices_and_normals();
+
+
+                break;
+        }    }
+
+    public Model(letter K)
+    {
+        switch (shape)
+        {
+            case letter.K:
+                initialize_lists();
+                add_letter_K_vertices();
+                add_letter_K_texture_coordinates();
+                add_letter_K-indices_and_normals();
+
+
+                break;
+        }
+    }
+
+    //Add in sides
+    private void add_texture_coordinates()
+    {
+        _texture_coordinates.Add(new Vector2(0.4f, 0));//Bottom
+        _texture_coordinates.Add(new Vector2(0.5f, 0));
+        _texture_coordinates.Add(new Vector2(0.6f, 0));
+        _texture_coordinates.Add(new Vector2(0.7f, 0));
+
+        _texture_coordinates.Add(new Vector2(0.0f, 0.1f));//Lower section
+        _texture_coordinates.Add(new Vector2(0.1f, 0.1f));
+        _texture_coordinates.Add(new Vector2(0.2f, 0.1f));
+        _texture_coordinates.Add(new Vector2(0.3f, 0.1f));
+        _texture_coordinates.Add(new Vector2(0.4f, 0.1f));
+        _texture_coordinates.Add(new Vector2(0.5f, 0.1f));
+        _texture_coordinates.Add(new Vector2(0.6f, 0.1f));
+        _texture_coordinates.Add(new Vector2(0.7f, 0.1f));
+        _texture_coordinates.Add(new Vector2(0.8f, 0.1f));
+
+        _texture_coordinates.Add(new Vector2(0.2f, 0.3f));
+        _texture_coordinates.Add(new Vector2(0.3f, 0.3f));
+        _texture_coordinates.Add(new Vector2(0.4f, 0.3f));
+        _texture_coordinates.Add(new Vector2(0.5f, 0.3f));
+        _texture_coordinates.Add(new Vector2(0.6f, 0.3f));
+        _texture_coordinates.Add(new Vector2(0.7f, 0.3f));
+
+        _texture_coordinates.Add(new Vector2(0.2f, 0.4f));//Centre
+        _texture_coordinates.Add(new Vector2(0.3f, 0.4f));
+        _texture_coordinates.Add(new Vector2(0.4f, 0.4f));
+        _texture_coordinates.Add(new Vector2(0.5f, 0.4f));
+        _texture_coordinates.Add(new Vector2(0.6f, 0.4f));
+        _texture_coordinates.Add(new Vector2(0.7f, 0.4f));
+
+        _texture_coordinates.Add(new Vector2(0.0f, 0.6f));
+        _texture_coordinates.Add(new Vector2(0.1f, 0.6f));
+        _texture_coordinates.Add(new Vector2(0.2f, 0.6f));
+        _texture_coordinates.Add(new Vector2(0.3f, 0.6f));
+        _texture_coordinates.Add(new Vector2(0.4f, 0.6f));
+        _texture_coordinates.Add(new Vector2(0.5f, 0.6f));
+        _texture_coordinates.Add(new Vector2(0.6f, 0.6f));
+        _texture_coordinates.Add(new Vector2(0.7f, 0.6f));
+        _texture_coordinates.Add(new Vector2(0.8f, 0.6f)); 
+    }
+
+    private void add_indices_and_normals()
+    {
+        //
+        _index_list.Add(0);     _texture_index_list.Add(32);
+        _index_list.Add(1);     _texture_index_list.Add(31);   _face_normals.Add(new Vector3(0,0,-1));
+        _index_list.Add(2);     _texture_index_list.Add(23); // 
+        _index_list.Add(0);     _texture_index_list.Add(32);
+        _index_list.Add(2);     _texture_index_list.Add(23);    _face_normals.Add(new Vector3(0, 0, -1));
+        _index_list.Add(3);     _texture_index_list.Add(24); // 
+
+        _index_list.Add(2);     _texture_index_list.Add(24);
+        _index_list.Add(3);     _texture_index_list.Add(23); _face_normals.Add(new Vector3(0, 0, -1));
+        _index_list.Add(8);     _texture_index_list.Add(17); // 
+        _index_list.Add(2);     _texture_index_list.Add(24);
+        _index_list.Add(7);     _texture_index_list.Add(18); _face_normals.Add(new Vector3(0, 0, -1));
+        _index_list.Add(8);     _texture_index_list.Add(17); // 
+
+        _index_list.Add(7);     _texture_index_list.Add(18);
+        _index_list.Add(8);     _texture_index_list.Add(17); _face_normals.Add(new Vector3(0,0, -1));
+        _index_list.Add(9);     _texture_index_list.Add(12); // 
+        _index_list.Add(8);     _texture_index_list.Add(17);
+        _index_list.Add(9);     _texture_index_list.Add(12); _face_normals.Add(new Vector3(0, 0, -1));
+        _index_list.Add(10);     _texture_index_list.Add(11); // 
+
+        //
+        _index_list.Add(4);     _texture_index_list.Add(30);
+        _index_list.Add(5);     _texture_index_list.Add(29); _face_normals.Add(new Vector3(0, 0, -1));
+        _index_list.Add(3);     _texture_index_list.Add(23); // 
+        _index_list.Add(5);     _texture_index_list.Add(29);
+        _index_list.Add(6);     _texture_index_list.Add(22); _face_normals.Add(new Vector3(0, 0, -1));
+        _index_list.Add(3);     _texture_index_list.Add(23);// 
+
+        _index_list.Add(3);     _texture_index_list.Add(23);
+        _index_list.Add(6);     _texture_index_list.Add(22); _face_normals.Add(new Vector3(0, 0, -1));
+        _index_list.Add(11);     _texture_index_list.Add(16); //
+        _index_list.Add(3);     _texture_index_list.Add(23);
+        _index_list.Add(11);     _texture_index_list.Add(16); _face_normals.Add(new Vector3(0, 0, -1));
+        _index_list.Add(8);     _texture_index_list.Add(17); // 
+
+        _index_list.Add(8);     _texture_index_list.Add(17);
+        _index_list.Add(11);     _texture_index_list.Add(16); _face_normals.Add(new Vector3(0, 0, -1));
+        _index_list.Add(13);     _texture_index_list.Add(9); 
+        _index_list.Add(8);     _texture_index_list.Add(17);
+        _index_list.Add(13);     _texture_index_list.Add(9); _face_normals.Add(new Vector3(0, 0, -1));
+        _index_list.Add(12);     _texture_index_list.Add(10);
+
+        //TOPS 
+        _index_list.Add(0); _texture_index_list.Add(32); 
+        _index_list.Add(1); _texture_index_list.Add(31); _face_normals.Add(new Vector3(0, 1, 0));
+        _index_list.Add(14); _texture_index_list.Add(36); 
+        _index_list.Add(0); _texture_index_list.Add(32);
+        _index_list.Add(14); _texture_index_list.Add(36); _face_normals.Add(new Vector3(0, 1, 0));
+        _index_list.Add(15); _texture_index_list.Add(35);
+
+        _index_list.Add(4); _texture_index_list.Add(30);
+        _index_list.Add(5); _texture_index_list.Add(29); _face_normals.Add(new Vector3(0, 1, 0));
+        _index_list.Add(19); _texture_index_list.Add(33);
+        _index_list.Add(4); _texture_index_list.Add(30);
+        _index_list.Add(19); _texture_index_list.Add(33); _face_normals.Add(new Vector3(0, 1, 0));
+        _index_list.Add(18); _texture_index_list.Add(34);
+
+        //BOTTOMS                                             
+        _index_list.Add(9); _texture_index_list.Add(12);
+        _index_list.Add(10); _texture_index_list.Add(11); _face_normals.Add(new Vector3(0, 1, 0));
+        _index_list.Add(24); _texture_index_list.Add(3);
+        _index_list.Add(9); _texture_index_list.Add(12);
+        _index_list.Add(23); _texture_index_list.Add(3); _face_normals.Add(new Vector3(0, 1, 0));
+        _index_list.Add(24); _texture_index_list.Add(4);
+
+        _index_list.Add(12); _texture_index_list.Add(10);
+        _index_list.Add(13); _texture_index_list.Add(9); _face_normals.Add(new Vector3(0, 1, 0));
+        _index_list.Add(27); _texture_index_list.Add(1);
+        _index_list.Add(12); _texture_index_list.Add(10);
+        _index_list.Add(27); _texture_index_list.Add(1); _face_normals.Add(new Vector3(0, 1, 0));
+        _index_list.Add(28); _texture_index_list.Add(2);
+
+        //LEFT SIDE
+        _index_list.Add(5); _texture_index_list.Add(29);
+        _index_list.Add(19); _texture_index_list.Add(28); _face_normals.Add(new Vector3(-1, 0, 0));
+        _index_list.Add(20); _texture_index_list.Add(21);
+        _index_list.Add(5); _texture_index_list.Add(29);
+        _index_list.Add(20); _texture_index_list.Add(21); _face_normals.Add(new Vector3(-1, 0, 0));
+        _index_list.Add(6); _texture_index_list.Add(22);
+
+        _index_list.Add(6); _texture_index_list.Add(22);
+        _index_list.Add(20); _texture_index_list.Add(21); _face_normals.Add(new Vector3(-1, 0, 0));
+        _index_list.Add(25); _texture_index_list.Add(16);
+        _index_list.Add(6); _texture_index_list.Add(22);
+        _index_list.Add(25); _texture_index_list.Add(15); _face_normals.Add(new Vector3(-1, 0, 0));
+        _index_list.Add(11); _texture_index_list.Add(16);
+
+        _index_list.Add(11); _texture_index_list.Add(16);
+        _index_list.Add(25); _texture_index_list.Add(15); _face_normals.Add(new Vector3(-1, 0, 0));
+        _index_list.Add(27); _texture_index_list.Add(8);
+        _index_list.Add(11); _texture_index_list.Add(16);
+        _index_list.Add(27); _texture_index_list.Add(8); _face_normals.Add(new Vector3(-1, 0, 0));
+        _index_list.Add(13); _texture_index_list.Add(9);
+
+        //RIGHT SIDE
+        _index_list.Add(4); _texture_index_list.Add(30);
+        _index_list.Add(18); _texture_index_list.Add(27); _face_normals.Add(new Vector3(1, 0, 0));
+        _index_list.Add(17); _texture_index_list.Add(20);
+        _index_list.Add(4); _texture_index_list.Add(30);
+        _index_list.Add(17); _texture_index_list.Add(20); _face_normals.Add(new Vector3(1, 0, 0));
+        _index_list.Add(3); _texture_index_list.Add(23);
+
+        _index_list.Add(8); _texture_index_list.Add(17);
+        _index_list.Add(22); _texture_index_list.Add(14); _face_normals.Add(new Vector3(1, 0, 0));
+        _index_list.Add(26); _texture_index_list.Add(7);
+        _index_list.Add(8); _texture_index_list.Add(17);
+        _index_list.Add(26); _texture_index_list.Add(7); _face_normals.Add(new Vector3(1, 0, 0));
+        _index_list.Add(12); _texture_index_list.Add(10);
+
+        //Inside diagonal-----------------------------------
+        _index_list.Add(1); _texture_index_list.Add(31);
+        _index_list.Add(15); _texture_index_list.Add(27); _face_normals.Add(new Vector3(-1, 0, 0));
+        _index_list.Add(17); _texture_index_list.Add(20);
+        _index_list.Add(1); _texture_index_list.Add(31);
+        _index_list.Add(17); _texture_index_list.Add(20); _face_normals.Add(new Vector3(-1, 0, 0));
+        _index_list.Add(3); _texture_index_list.Add(23);
+
+        _index_list.Add(8); _texture_index_list.Add(17);//Wrong section?
+        _index_list.Add(22); _texture_index_list.Add(14); _face_normals.Add(new Vector3(-1, 0, 0));
+        _index_list.Add(24); _texture_index_list.Add(6);
+        _index_list.Add(8); _texture_index_list.Add(17);
+        _index_list.Add(24); _texture_index_list.Add(6); _face_normals.Add(new Vector3(-1, 0, 0));
+        _index_list.Add(10); _texture_index_list.Add(11);
+
+        //Outside diagonal top
+        _index_list.Add(0); _texture_index_list.Add(32);
+        _index_list.Add(14); _texture_index_list.Add(25); _face_normals.Add(new Vector3(1, 0, 0));
+        _index_list.Add(16); _texture_index_list.Add(19);
+        _index_list.Add(0); _texture_index_list.Add(32);
+        _index_list.Add(16); _texture_index_list.Add(19); _face_normals.Add(new Vector3(1, 0, 0));
+        _index_list.Add(2); _texture_index_list.Add(16);
+
+        //Right centre
+        _index_list.Add(2); _texture_index_list.Add(24);
+        _index_list.Add(7); _texture_index_list.Add(18); _face_normals.Add(new Vector3(1, 0, 0));
+        _index_list.Add(21); _texture_index_list.Add(13);
+        _index_list.Add(2); _texture_index_list.Add(24);
+        _index_list.Add(21); _texture_index_list.Add(13); _face_normals.Add(new Vector3(1, 0, 0));
+        _index_list.Add(7); _texture_index_list.Add(19);
+
+        //Outside diagonal bottom
+        _index_list.Add(7); _texture_index_list.Add(18);
+        _index_list.Add(21); _texture_index_list.Add(13); _face_normals.Add(new Vector3(1, 0, 0));
+        _index_list.Add(23); _texture_index_list.Add(5);
+        _index_list.Add(7); _texture_index_list.Add(18);
+        _index_list.Add(23); _texture_index_list.Add(5); _face_normals.Add(new Vector3(1, 0, 0));
+        _index_list.Add(9); _texture_index_list.Add(12);
+    }
+
+    private void initialize_lists()
+    {
+        _vertices = new List<Vector3>();
+        _index_list = new List<int>();
+        _texture_coordinates = new List<Vector2>();
+        _texture_index_list = new List<int>();
+        _face_normals = new List<Vector3>();
+    }
+
+        //indices
+    private void add_letter_K_indices()
+    {
+        //Right
+        _index_list.Add(0); 
+        _index_list.Add(1);
+        _index_list.Add(2);
+        _index_list.Add(0); 
+        _index_list.Add(2); 
+        _index_list.Add(3); 
+
+        _index_list.Add(2); 
+        _index_list.Add(3); 
+        _index_list.Add(7); 
+        _index_list.Add(2);
+        _index_list.Add(7); 
+        _index_list.Add(8); 
+
+        _index_list.Add(7); 
+        _index_list.Add(8); 
+        _index_list.Add(9); 
+        _index_list.Add(8);
+        _index_list.Add(9); 
+        _index_list.Add(10); 
+    
+        //Left
+        _index_list.Add(4); 
+        _index_list.Add(5);
+        _index_list.Add(3); 
+        _index_list.Add(5); 
+        _index_list.Add(6); 
+        _index_list.Add(3);
+
+        _index_list.Add(3); 
+        _index_list.Add(6); 
+        _index_list.Add(11); 
+        _index_list.Add(3); 
+        _index_list.Add(11); 
+        _index_list.Add(8); 
+
+        _index_list.Add(8); 
+        _index_list.Add(11); 
+        _index_list.Add(13); 
+        _index_list.Add(8); 
+        _index_list.Add(13);
+        _index_list.Add(12); 
+
+
+
+
+
+
+    }
+}
+    private void add_letter_K_vertices() {
+    _vertices.Add(new Vector3(0, 0, -1));//bottom left front
+    _vertices.Add(new Vector3(1, 0, -1));
+    _vertices.Add(new Vector3(0, 2, -1));//centre left front
+    _vertices.Add(new Vector3(1, 2, -1));
+    _vertices.Add(new Vector3(0, 3, -1));//centre left front
+    _vertices.Add(new Vector3(1, 3, -1));
+    _vertices.Add(new Vector3(0, 5, -1));//top left front
+    _vertices.Add(new Vector3(1, 5, -1));
+
+    _vertices.Add(new Vector3(2, 0, -1));//bottom right front
+    _vertices.Add(new Vector3(2, 5, -1));//top right front
+    _vertices.Add(new Vector3(3, 0, -1));//bottom right front
+    _vertices.Add(new Vector3(3, 5, -1));//top right front
+    _vertices.Add(new Vector3(2, 2, -1));//centre right front
+    _vertices.Add(new Vector3(2, 3, -1));//centre right front
+
+    _vertices.Add(new Vector3(0, 0, 1));//bottom left back
+    _vertices.Add(new Vector3(1, 0, 1));
+    _vertices.Add(new Vector3(0, 2, 1));//centre left back
+    _vertices.Add(new Vector3(1, 2, 1));
+    _vertices.Add(new Vector3(0, 3, 1));//centre left back
+    _vertices.Add(new Vector3(1, 3, 1));
+    _vertices.Add(new Vector3(0, 5, 1));//top left back
+    _vertices.Add(new Vector3(1, 5, 1));
+
+    _vertices.Add(new Vector3(2, 0, 1));//bottom right back
+    _vertices.Add(new Vector3(2, 5, 1));//top right back
+    _vertices.Add(new Vector3(3, 0, 1));//bottom right back
+    _vertices.Add(new Vector3(3, 5, 1));//top right back
+    _vertices.Add(new Vector3(2, 2, 1));//centre right back
+    _vertices.Add(new Vector3(2, 3, 1));//centre right back
+
+
+
+}
---
+++ 3d Graphics/Assets/Pipeline.cs
@@ -0,0 +1,68 @@
+﻿/*
+ * Fill values into the model class for your letter.
+You will need to change the accessibility of the lists in model to internal or public
+Call the method, passing your model into it, (in start)
+Import the texture you wish to use
+Run unity, while running attach the texture to the newly created gameobject in the scene hierarchy
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipeline : MonoBehaviour
+{
+    Matrix4x4 translation, rotation, scale, viewing, projection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Model cube = new Model(Model.default_primitives.Cube);
+
+        CreateUnityGameObject(cube);
+
+        Model k = new Model(Model.Letter.K);
+        CreateUnityGameObject(K);
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public GameObject CreateUnityGameObject(Model model)
+    {
+        Mesh mesh = new Mesh();
+        GameObject newGO = new GameObject();
+        MeshFilter mesh_filter = newGO.AddComponent<MeshFilter>();
+        MeshRenderer mesh_renderer = newGO.AddComponent<MeshRenderer>();
+
+
+
+
+        List<Vector3> coords = new List<Vector3>();
+        List<int> dummy_indices = new List<int>();
+        List<Vector2> text_coords = new List<Vector2>();
+        List<Vector3> normals = new List<Vector3>();
+
+        for (int i = 0; i <= model._index_list.Count - 3; i = i + 3)
+        {
+            Vector3 normal_for_face = model._face_normals[i / 3];
+            normal_for_face = new Vector3(normal_for_face.x, normal_for_face.y, -normal_for_face.z);
+            coords.Add(model._vertices[model._index_list[i]]); dummy_indices.Add(i); text_coords.Add(model._texture_coordinates[model._texture_index_list[i]]); normals.Add(normal_for_face);
+            coords.Add(model._vertices[model._index_list[i + 1]]); dummy_indices.Add(i + 1); text_coords.Add(model._texture_coordinates[model._texture_index_list[i + 1]]); normals.Add(normal_for_face);
+            coords.Add(model._vertices[model._index_list[i + 2]]); dummy_indices.Add(i + 2); text_coords.Add(model._texture_coordinates[model._texture_index_list[i + 2]]); normals.Add(normal_for_face);
+        }
+
+        mesh.vertices = coords.ToArray();
+        mesh.triangles = dummy_indices.ToArray();
+        mesh.uv = text_coords.ToArray();
+        mesh.normals = normals.ToArray(); ;
+
+        mesh_filter.mesh = mesh;
+        return newGO;
+
+    }
+}
